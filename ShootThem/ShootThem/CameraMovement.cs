deb0e7d0b2c3054a259e89a881b7314d18decc1f
felix <felix.lundh@gmail.com>
2020-10-23 17:21:50 +0200

    map now rendered

---
+++ ShootThem/ShootThem/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+
+namespace ShootThem {
+    public class CameraMovement {
+        public CameraMovement() {
+        }
+        public void update(OrthographicCamera camera, GameTime gameTime) {
+            move(camera, gameTime);
+        }
+        private void move(OrthographicCamera camera, GameTime gameTime) {
+            float deltaTime = gameTime.GetElapsedSeconds();
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Up)) {
+                camera.Move(new Vector2(0, -speed) * deltaTime);
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Down)) {
+                camera.Move(new Vector2(0, +speed) * deltaTime);
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Left)) {
+                camera.Move(new Vector2(-speed, 0) * deltaTime);
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Right)) {
+                camera.Move(new Vector2(+speed, 0) * deltaTime);
+            }
+        }
+
+        private const float speed = 300;
+    }
+}
--- ShootThem/ShootThem/Game1.cs
+++ ShootThem/ShootThem/Game1.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+// To get the camera
+using MonoGame.Extended;
+// Then you need a viewport adapter
+using MonoGame.Extended.ViewportAdapters;
 
 namespace ShootThem {
     public class Game1 : Game {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private OrthographicCamera camera;
+        private CameraMovement _cameraMovement;
+        private TiledMap map;
+        private TiledMapRenderer _mapRenderer;
 
         public Game1() {
             _graphics = new GraphicsDeviceManager(this);
@@ -14,7 +24,18 @@ namespace ShootThem {
         }
 
         protected override void Initialize() {
-            // TODO: Add your initialization logic here
+            // TODO: Add your initialization logic
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+
+            map = Content.Load<TiledMap>("test");
+            _mapRenderer = new TiledMapRenderer(GraphicsDevice, map);
+
+            var viewportadapter = new BoxingViewportAdapter(Window, GraphicsDevice, 400, 400);
+            camera = new OrthographicCamera(viewportadapter);
+            camera.LookAt(new Vector2(0, 0));
+            _cameraMovement = new CameraMovement();
 
             base.Initialize();
         }
@@ -30,6 +51,9 @@ namespace ShootThem {
                 Exit();
 
             // TODO: Add your update logic here
+            _mapRenderer.Update(gameTime);
+
+            _cameraMovement.update(camera, gameTime);
 
             base.Update(gameTime);
         }
@@ -38,6 +62,12 @@ namespace ShootThem {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin(transformMatrix: camera.GetViewMatrix(), samplerState: new SamplerState { Filter = TextureFilter.Point});
+
+            _mapRenderer.Draw(camera.GetViewMatrix());
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
